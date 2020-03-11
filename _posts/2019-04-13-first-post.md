49f26fb86971db1ea8bfd9b503c810eae3ded889
John Sun <johnsun@Johnui-MacBookPro.local>
2020-03-11 21:58:45 +0900

    a

--- _posts/2019-04-13-first-post.md
+++ _posts/2019-04-13-first-post.md
@@ -2,6 +2,9 @@
 title:  "github.io 블로그 시작하기"
 excerpt: "GitHub Blog 서비스인 github.io 블로그 시작하기로 했다."
 
+header:
+  teaser: /assets/images/teaser_test_image.jpg
+
 categories:
   - Blog
 tags:
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,17 +3,17 @@ var store = [{
         "excerpt":"GitHub Blog 서비스인 github.io 블로그 시작하기로 했다. GitHub Blog 서비스의 이름은 Pages이다. Pages가 다른 블로그 플랫폼 보다 편한 것 같아서 마음에 든다. 다른 사람들도 같이 많이 사용했으면 좋겠다는 생각이 든다. YFM에서 정의한 제목을 이중 괄호 구문으로 본문에 추가할 수 있다. 이 글의 제목은 github.io 블로그 시작하기이고 마지막으로 수정된 시간은 2019-04-13...","categories": ["Blog"],
         "tags": ["Blog"],
         "url": "http://localhost:4000/blog/first-post/",
-        "teaser": null
+        "teaser": "http://localhost:4000/assets/images/teaser_test_image.jpg"
       },{
-        "title": "GBC 과제 1",
-        "excerpt":"GBC 과제 1 1. CVE란 무엇인가 CVE : ‘Common Vulnerabilities and Exposure’ 의 약자로 정보 보안 취약점 표준 코드를 뜻한다 현재까지 알려진 취약점을 식별하는 방식을 표준화하는 데 목적이 있는 코드이다 표기 방식은 “CVE-[취약점이 발견된 년도]-[취약점고유번호]”이다 최근 2년 내에 발견 된 CVE 2가지 예시 CVE-2019-0797(Win32k 권한 상승 취약점) : Kaspersky 보안...","categories": ["GBC"],
+        "title": "GBC 보안 과제 1",
+        "excerpt":"GBC 보안 과제 1 1. CVE란 무엇인가 CVE : ‘Common Vulnerabilities and Exposure’ 의 약자로 정보 보안 취약점 표준 코드를 뜻한다 현재까지 알려진 취약점을 식별하는 방식을 표준화하는 데 목적이 있는 코드이다 표기 방식은 “CVE-[취약점이 발견된 년도]-[취약점고유번호]”이다 최근 2년 내에 발견 된 CVE 2가지 예시 CVE-2019-0797(Win32k 권한 상승 취약점) : Kaspersky...","categories": ["GBC"],
         "tags": ["GBC"],
         "url": "http://localhost:4000/gbc/gbc1/",
-        "teaser": null
+        "teaser": "http://localhost:4000/assets/images/profile_image.jpg"
       },{
         "title": "용어 및 개념 정리",
-        "excerpt":"**gcc: ** GNU Compiler Collection의 약자로 GNU 프로젝트의 일환으로 개발되어 널리 쓰이고 있는 컴파일러이다. 원래는 GNU C Compiler의 약자였지만 이후에 C++, JAVA, Fortran 등 여러 언어를 컴파일 할 수 있게 되면서 현재의 이름으로 바뀌게 되었다. **homebrew: ** 개발자들이 쉽게 프로그램을 검색, 설치, 삭제할 수 있게 해주는 맥 전용 패키지 매니저....","categories": ["Blog"],
+        "excerpt":"gcc: GNU Compiler Collection의 약자로 GNU 프로젝트의 일환으로 개발되어 널리 쓰이고 있는 컴파일러이다. 원래는 GNU C Compiler의 약자였지만 이후에 C++, JAVA, Fortran 등 여러 언어를 컴파일 할 수 있게 되면서 현재의 이름으로 바뀌게 되었다. homebrew: 개발자들이 쉽게 프로그램을 검색, 설치, 삭제할 수 있게 해주는 맥 전용 패키지 매니저. Ex) 앱...","categories": ["Blog"],
         "tags": ["Blog"],
         "url": "http://localhost:4000/blog/words-organize/",
-        "teaser": null
+        "teaser": "http://localhost:4000/assets/images/profile_image.jpg"
       }]
